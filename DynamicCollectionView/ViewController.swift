b75f75540f68082c0a0fa0c056ddc7892747e35f
Hubert Wang <hubert.tatra@gmail.com>
2019-03-24 20:06:39 +0700

    Initial Commit

---
+++ DynamicCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicCollectionView
+//
+//  Created by Hubert Wang on 24/03/19.
+//  Copyright © 2019 Hubert Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
